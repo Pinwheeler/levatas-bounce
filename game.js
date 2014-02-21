cd4904088574e0a6968b109c70b3d6300373827d
Anthony Dreessen <anthonydreessen@gmail.com>
2014-02-21 09:12:50 -0500

    Update game.js

--- game.js
+++ game.js
@@ -117,7 +117,7 @@ var GameLoop = function(){
 	ctx.fillText("POINTS:" + points, 10, height-10);
 	//and add text in the left-bottom corner of the canvas
 	ctx.fillText("TILT: " + globalTilt, 10, height-30);
-	ctx.fillText("VEL: " + player.v, 10, height-20);
+	ctx.fillText("VEL: " + player.xVel, 10, height-20);
 
 	if (state)
         gLoop = setTimeout(GameLoop, 1000 / 60);
